e7af5661e8a7abc462a360b999813209458d12a7
Fortune505 <forturn397@gmail.com>
2026-03-31 13:09:01 +0800

    Добавьте файлы проекта.

---
+++ EvantHandling/Form1.cs
@@ -0,0 +1,52 @@
+using EvantHandling.Objects;
+using System.Security.Cryptography.Xml;
+
+namespace EvantHandling
+{
+    public partial class Form1 : Form
+    {
+        MyRectangle myRect;
+        List<BaseObject> objects = new();
+        Player player;
+        Marker marker;
+        public Form1()
+        {
+            InitializeComponent();
+            player = new Player(pbMain.Width / 2, pbMain.Height / 2, 0);
+            marker = new Marker(pbMain.Width / 2 + 50, pbMain.Handle / 2 + 50, 0);
+            objects.Add(marker);
+            objects.Add(player);
+            objects.Add(new MyRectangle(50, 50, 0));
+            objects.Add(new MyRectangle(100, 100, 45));
+
+        }
+
+        private void pbMain_Paint(object sender, PaintEventArgs e)
+        {
+            var g = e.Graphics;
+            g.Clear(Color.White);
+
+            foreach (var obj in objects)
+            {
+                g.Transform = obj.GetTransform();
+                obj.Render(g);
+            }
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            float dx = marker.X - player.X;
+            float dy = marker.Y - player.Y;
+
+            float length = MathF.Sqrt(dx * dx + dy * dy);
+            dx /= length;
+            dy /= length;
+
+            player.X += dx * 2;
+            player.Y += dy * 2;
+
+            pbMain.Invalidate();
+        }
+    }
+}
